1baa61cafdee74a65f0263963bb88bb2ac6fc6bd
matejxxx <matej.crepinsek@gmail.com>
2012-02-17 11:45:11 +0100

    add all

---
+++ BrisiMe123x/src/brisi/packag2/name/BrisiMe123xActivity.java
@@ -0,0 +1,13 @@
+package brisi.packag2.name;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BrisiMe123xActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
